6d10deb324c23a67b4c32606e7d10668de09e753
Captain Capslock and 7 others <Rickvs64@users.noreply.github.com>
2019-12-11 11:42:58 +0100

    Add IsPalindrome check

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -18,13 +18,18 @@ namespace Palindrome
         /// </summary>
         /// <param name="input">Input string to be reversed.</param>
         /// <returns>Same as input string but in reverse.</returns>
-        String reverseString(String input)
+        String ReverseString(String input)
         {
             // Convert input string into a character array.
             char[] chars = input.ToCharArray();
             Array.Reverse(chars);
             // String class has a constructor to convert a char array to string.
             return new string(chars);
+        }
+
+        bool IsPalindrome(String input)
+        {
+            return (input == reverseString(input));
         }
     }
 }
