dc93a34de2cff600dff39ddfaceec45753c38f4b
Captain Capslock and 7 others <Rickvs64@users.noreply.github.com>
2019-12-11 11:40:16 +0100

    Add ReverseString() method

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -2,11 +2,29 @@
 
 namespace Palindrome
 {
+    /// <summary>
+    /// This application is based on Project Euler's palindrome problem.
+    /// https://projecteuler.net/problem=4
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        /// Reverse a given string.
+        /// </summary>
+        /// <param name="input">Input string to be reversed.</param>
+        /// <returns>Same as input string but in reverse.</returns>
+        String reverseString(String input)
+        {
+            // Convert input string into a character array.
+            char[] chars = input.ToCharArray();
+            Array.Reverse(chars);
+            // String class has a constructor to convert a char array to string.
+            return new string(chars);
         }
     }
 }
